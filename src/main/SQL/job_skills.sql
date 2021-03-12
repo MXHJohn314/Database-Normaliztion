fa9207711eafda5a95f0909d25e23c184fa43b90
Galacticwafer <Galacticwafer@DESKTOP-0AIPIP5>
2021-03-11 21:51:52 -0700

    Updated to use prepared batch statements and sql injection.
    Added job_skills.sql

---
+++ src/main/SQL/job_skills.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS JOBS;
+DROP USER IF EXISTS 'job_skills'@'%';
+DROP USER IF EXISTS 'job_skills_admin'@'%';
+CREATE DATABASE IF NOT EXISTS JOBS;
+USE JOBS;
+
+create table jobs (
+	id int primary key,
+	title varchar(255) not null
+);
+
+create table skills (
+	id int primary key,
+	name varchar(255) not null
+);
+
+create table job_skills (
+	job_id int not null,
+	skill_id int not null,
+	constraint pk_job_skills  primary key (job_id, skill_id),
+	foreign key (job_id)	    references   jobs	(id),
+	foreign key (skill_id)     references   skills	(id)
+);
+
+-- Todo Check for 3NF. 
+-- Todod There should be 2 database users:
+--      job_skills (with only SELECT capabilities on all tables) and
+--      job_skills_admin (with full capabilities on all tables). 
+CREATE USER 'job_skills'@'%';
+GRANT SELECT ON jobs.* TO 'job_skills'@'%';
+CREATE USER 'job_skills_admin'@'%';
+GRANT ALL ON *.* TO 'job_skills_admin'@'%';
+
+-- Todod a) what is the total number of job positions?
+select count(id)from jobs;
+
+-- Todod b) what is the total number of skills?
+select count(id)from skills; 
+
+-- Todod c) which job position titles have the word "database"?
+select title from jobs where title like '%database%';
+
+-- Todod d) provide an alphabetical list of all job position titles that require "sql" or "mysql" as a skill.
+select j.title from jobs j
+inner join job_skills js on j.id = js.job_id
+inner join skills s on js.skill_id = s.id
+where s.name in ('sql', 'mysql')
+order by 1
+;
+
+-- Todo e) which skills "database analyst"-like positions have that "database admin"-like positions don't? 
+--  In other words, select the jobs that require at least one skill that has "database analyst" somewhere in the skill description, but but do not require any skills that have "database admin" somewhere in the skill discription
+
+-- Todod f) list the top 20 skills required by job positions having the word "database" in their titles.
+select s2.name as 'skills',count(js.skill_id) 'times skill occurred in job 
+postings'
+from job_skills js
+inner join skills s2
+on js.skill_id = s2.id 
+       and s2.name like '%database%'
+group by js.skill_id
+order by js.skill_id
+limit 20
+;
+
+-- Todo Reflection
--- src/main/java/JobSkills.java
+++ src/main/java/JobSkills.java
@@ -2,155 +2,115 @@
  * CS3810 - Principles of Database Systems - Spring 2021
  * Instructor: Thyago Mota
  * Description: DB 02 - JobSkills
- * Student(s) Name(s): Malcolm Johnson, Dakota Miller, Adam Wojdyla
+ * Student(s) Name(s):
  */
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-import java.util.Scanner;
+import java.sql.*;
+import java.util.*;
 
 public class JobSkills {
-	public static final int FILE_NAME_INDEX = 0;
-	public static final int TABLE_NAME_INDEX = 1;
-	private static Connection connection;
-	private static String DB_NAME;
-	public static String DATASET = "data/job_skills.csv";
-	public static int SQL_ITER = 0;
-	public static final String[] SQL_COMMANDS = new String[] {
-	 "drop database if exists jobs;",
-	 
-	 "create database if not exists jobs;",
-	 
-	 "use jobs;",
-	 
-	 "create table jobs (" +
-	 "ID int primary key" +
-	 ",descr varchar(255) not null" +
-	 ")",
-	 
-	 "create table skills (" +
-	 "ID int primary key" +
-	 ",descr varchar(255) not null" +
-	 ")",
-	 
-	 "create table job_skills (" +
-	 "jobID int not null" +
-	 ",skillID int not null" +
-	 ",constraint pk_job_skills primary key (jobID, skillID)" +
-	 ",foreign key (jobID)		references   jobs	(ID)" +
-	 ",foreign key (skillID)    references   skills	(ID)" +
-	 ") ",
-	 
-	 "SET GLOBAL local_infile=1",
-	 };
-	
-	public static void main(String[] args)
-	throws IOException, SQLException, ClassNotFoundException {
-		
-		String userDir = System.getProperty("user.dir");
-		// TODO: load database properties
-		Properties prop = loadDatabaseProperties(
-		 userDir + "/config.properties");
-		
-		// TODO: connect to the database
-		getConnection(prop);
-		
-		// TODO: complete the data load
-		//Files.delete(Paths.get(PROGRAM_DATA_FILE));
-		for(String sql: SQL_COMMANDS) {
-			System.out.println(sql);
-			connection.createStatement().executeUpdate(sql);
-		}
-		String[][] tables = splitCsv(userDir + "/data/", "jobs.csv");
-		
-		for(String[] tableParams: tables) {
-			String sql = "LOAD DATA LOCAL INFILE ' "
-				+ tableParams[FILE_NAME_INDEX].replace("\\", "/") + "'"
-				+ " INTO TABLE " + tableParams[TABLE_NAME_INDEX]
-				+ " fields terminated by ','\n"
-				+ " optionally enclosed by '\"'\n"
-				+ " escaped by '\"'\n"
-				+ " LINES TERMINATED BY '\\r\\n'"
-				+ " IGNORE 1 LINES";
-			connection.createStatement().executeUpdate(sql);
-		}
-	}
-	
-	private static void getConnection(Properties prop)
-	throws ClassNotFoundException, SQLException {
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		DB_NAME = prop.getProperty("schema");
-		connection = DriverManager.getConnection(
-		 "jdbc:mysql://"
-		 + prop.getProperty("ip") + ":"
-		 + prop.getProperty("port")
-		 + "?allowLoadLocalInfile=true",
-		 prop.getProperty("username"),
-		 prop.getProperty("password")
-		);
-	}
-	
-	private static Properties loadDatabaseProperties(String propFileName)
-	throws IOException {
-		
-		Properties prop = new Properties();
-		InputStream stream = new FileInputStream(propFileName);
-		if(stream != null) {
-			prop.load(stream);
-		}
-		else {
-			throw new FileNotFoundException(
-			 "property file '" + propFileName + "' not found in the classpath");
-		}
-		return prop;
-	}
-	
-	public static String[][] splitCsv(String workingDir, String fileName)
-	throws IOException {
-		
-		File oldJobsFile = new File(workingDir + "jobs.csv");
-		Scanner sc = new Scanner(oldJobsFile);
-		File file1 = new File(workingDir + "jobs_updated.csv");
-		FileWriter jobsFileWriter =
-		 new FileWriter(file1);
-		File file2 = new File(workingDir + "skills.csv");
-		FileWriter skillsFileWriter =
-		 new FileWriter(file2);
-		File file3 = new File(workingDir + "job_skills.csv");
-		FileWriter jobSkillsFileWriter =
-		 new FileWriter(file3);
-		jobsFileWriter.write("ID,descr" + "\n");
-		skillsFileWriter.write("ID,descr" + "\n");
-		jobSkillsFileWriter.write("jobID,skillID," + "\n");
-		
-		while(sc.hasNextLine()) {
-			String line = sc.nextLine();
-			String[] jobAndRest = line.split(",");
-			String[] jobAndId = jobAndRest[0].split(":");
-			String jobId = jobAndId[0];
-			String jobTitle = jobAndId[1];
-			String[] skillsArray = jobAndRest[1].split(";");
-			jobsFileWriter.write(jobId + "," + jobTitle + "\n");
-			for(String skillAndId: skillsArray) {
-				String[] skill = skillAndId.split(":");
-				int skillId = Integer.parseInt(skill[0]);
-				String skillName = skill[1];
-				skillsFileWriter.write(skillId + "," + skillName + "\n");
-				jobSkillsFileWriter.write(jobId + "," + skillId + "\n");
-			}
-		}
-		sc.close();
-		jobsFileWriter.close();
-		skillsFileWriter.close();
-		jobSkillsFileWriter.close();
-		return new String[][] {
-		 new String[] {file1.getAbsolutePath(), "jobs"},
-		 new String[] {file2.getAbsolutePath(), "skills"},
-		 new String[] {file3.getAbsolutePath(), "job_skills"},
-		 };
-	}
+
+    public static String DATASET = "data/job_skills.csv";
+    public static String PROPERTIES = "config.properties";
+    private static Connection conn;
+    private static Statement stmt;
+
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+        // TODOd: load database properties
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(PROPERTIES));
+        String server = prop.getProperty("server");
+        String schema = prop.getProperty("database");
+        String username = prop.getProperty("user");
+        String password = prop.getProperty("password");
+
+        // TODOd: connect to the database
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://" + server + ":3306/" + schema ;
+        conn = DriverManager.getConnection(url,username, password);
+    
+        // Only for testing purposes
+         jdbcInit();
+    
+        // TODOd: complete the data load
+        splitCsv(System.getProperty("user.dir") + "/", DATASET);
+    }
+    
+    // Helper method for testing
+    private static void jdbcInit() throws SQLException {
+            String[] SQL_COMMANDS = new String[] {
+                "drop database if exists jobs",
+            
+                "create database if not exists jobs",
+            
+                "use jobs",
+            
+                "create table jobs (" +
+                        "id int primary key," +
+                        "title varchar(255) not null" +
+                        ")",
+            
+                "create table skills (" +
+                        "id int primary key," +
+                        "name varchar(255) not null" +
+                        ")",
+            
+                "create table job_skills (" +
+                        "job_id int not null," +
+                        "skill_id int not null," +
+                        "constraint pk_job_skills  primary key (job_id, skill_id)," +
+                        "foreign key (job_id)	    references   jobs	(id)," +
+                        "foreign key (skill_id)     references  skills	(id)" +
+                        ")",
+                };
+        for (int i = 0; i < SQL_COMMANDS.length; i++) {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(SQL_COMMANDS[i]);
+        }
+    }
+    
+    public static void splitCsv(String workingDir, String fileName) throws IOException, SQLException {
+        File oldJobsFile = new File(workingDir + DATASET);
+        Scanner sc = new Scanner(oldJobsFile);
+        PreparedStatement insertJobs = conn.prepareStatement("INSERT ignore INTO jobs (id, title) values (?, ?)");
+        PreparedStatement insertSkills = conn.prepareStatement("INSERT ignore INTO skills (id, name) values (?, ?)");
+        PreparedStatement insertJobSkills = conn.prepareStatement("INSERT ignore INTO job_skills (job_id, skill_id) values (?, ?)");
+        conn.setAutoCommit(false);
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] jobAndRest = line.split(",");
+            String[] jobAndId = jobAndRest[0].split(":");
+            int jobId = Integer.parseInt(jobAndId[0]);
+            String jobTitle = jobAndId[1];
+            String[] skillsArray = jobAndRest[1].split(";");
+            insertJobs.setInt(1, jobId);
+            insertJobs.setString(2,jobTitle);
+            insertJobs.addBatch();
+            for(String skillAndId: skillsArray) {
+                String[] skill = skillAndId.split(":");
+                int skillId = Integer.parseInt(skill[0]);
+                String skillName = skill[1];
+                insertSkills.setInt(1, skillId);
+                insertSkills.setString(2, skillName);
+                insertJobSkills.setInt(1, jobId);
+                insertJobSkills.setInt(2, skillId);
+                insertSkills.addBatch();
+                insertJobSkills.addBatch();
+            }
+        }
+        insertJobs.executeBatch();
+        insertSkills.executeBatch();
+        insertJobSkills.executeBatch();
+        conn.commit();
+        conn.setAutoCommit(true);
+    }
+
+    private static ResultSet query(String sql) throws SQLException {
+        return stmt.executeQuery(sql);
+    }
+
+    private static int update(String sql) throws SQLException {
+        return stmt.executeUpdate(sql);
+    }
 }
